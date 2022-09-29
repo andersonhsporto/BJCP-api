caceba83f76a4d027caf542d0bfb5e328b4e793f
andersonhsporto <anderson.higo2@gmail.com>
2022-09-29 01:22:12 -0300

    refactor: updateEntity

--- src/main/java/dev/anderson/bjcp/repositories/StyleRepository.java
+++ src/main/java/dev/anderson/bjcp/repositories/StyleRepository.java
@@ -1,6 +1,6 @@
 package dev.anderson.bjcp.repositories;
 
-import dev.anderson.bjcp.models.StyleEntity;
+import dev.anderson.bjcp.models.entities.StyleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StyleRepository extends JpaRepository<StyleEntity, Long> {
