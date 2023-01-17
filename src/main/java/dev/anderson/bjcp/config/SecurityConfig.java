44b7adbcc9708d02e626d92a7df7b9daa08f3ade
Anderson Porto <anderson.higo2@gmail.com>
2023-01-17 02:58:03 -0300

    add: spring security 6

---
+++ src/main/java/dev/anderson/bjcp/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package dev.anderson.bjcp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http.authorizeHttpRequests(authorizeConfig -> {
+          authorizeConfig.requestMatchers("/api/v1/styles").permitAll();
+          authorizeConfig.anyRequest().authenticated();
+        }).oauth2Login(Customizer.withDefaults())
+        .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt).build();
+  }
+
+
+}
