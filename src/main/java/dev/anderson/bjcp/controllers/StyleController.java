5ef735d72eb1f3c8776727ef696085baa4597e71
Anderson Porto <anderson.higo2@gmail.com>
2023-01-18 15:48:20 -0300

    refactor: move to security controller

--- src/main/java/dev/anderson/bjcp/controllers/StyleController.java
+++ src/main/java/dev/anderson/bjcp/controllers/StyleController.java
@@ -3,8 +3,6 @@ package dev.anderson.bjcp.controllers;
 import dev.anderson.bjcp.models.dto.StyleDto;
 import dev.anderson.bjcp.services.StyleService;
 import java.util.List;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +18,4 @@ public record StyleController(StyleService styleService) {
     return styleService.getAllStyles();
   }
 
-  @GetMapping("/jwt")
-  String jwt(@AuthenticationPrincipal Jwt jwt) {
-    return String.format("""
-				Principal: %s\n
-				Email attribute: %s\n
-				JWT: %s\n
-				""", jwt.getClaims(), jwt.getClaim("email"), jwt.getTokenValue());
-  }
 }
